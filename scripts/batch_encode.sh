6d42a715661cf075aa80030c5b80c4db9ab62312
Ben Brown <xthor@xthorsworld.com>
2021-01-20 23:40:19 -0700

    bug fixes

--- scripts/batch_encode.sh
+++ scripts/batch_encode.sh
@@ -6,7 +6,7 @@ startDate=$(date)
 encode_dir=/storage/videos/encoded
 
 # set handbrake profile info
-handbrake_preset_file="$(pwd)/../handbrake-presets/MKV-HQ.json"
+handbrake_preset_file="/home/xthor/git/ripper/handbrake-presets/MKV-HQ.json"
 handbrake_preset=$(cat "${handbrake_preset_file}" | jq -r '.PresetList[0].PresetName')
 
 log=$(mktemp -t makemkvcon.log.XXXX)
@@ -18,7 +18,7 @@ find . -maxdepth 1 -type f -iname "*.mkv" | while read inputfile; do
 		continue
 	fi
 	
-	echo "Processing ${inputfile} to ${output_file}..."
+	echo "Encoding ${inputfile} to ${output_file}..."
 	start=$(date +%s)
 	echo | HandBrakeCLI --preset-import-file "${handbrake_preset_file}" -Z "${handbrake_preset}" -i "${inputfile}" -o "${output_file}" 2> ${log}
     if [ $? -eq 0 ]; then
